2484ac163deacc368d2f1436b2f5645c1239235a
Bot Infinite <contact@infinitesparkbox.com>
2023-02-20 17:26:31 +0100

    2 ways to sort with bubble sort

--- playground/sort/level-1/1-bubble.c
+++ playground/sort/level-1/1-bubble.c
@@ -1,12 +1,30 @@
 #include <stdio.h>
 
-int main()
+void bubble_md_1(char cards[], int size)
 {
-    char cards[5] = { '2', '3', '4', '5', '1' };
     int i, j, tmp;
-    for (i = 0; i < 5 - 1 ; i++)
+
+    for (i = 0; i < size - 1; i++)
     {
-        for (j = 0; j < 5 - i - 1; j++)
+        for (j = i + 1; j < size; j++)
+        {
+            if (cards[i] > cards[j])
+            {
+                tmp = cards[i];
+                cards[i] = cards[j];
+                cards[j] = tmp;
+            }
+        }
+    }
+}
+
+void bubble_md_2(char cards[], int size)
+{
+    int i, j, tmp;
+
+    for (i = 0; i < size - 1 ; i++)
+    {
+        for (j = 0; j < size - i - 1; j++)
         {
             if (cards[j] > cards[j + 1])
             {
@@ -16,6 +34,16 @@ int main()
             }
         }
     }
+}
+
+int main()
+{
+    char cards[5] = { '2', '3', '4', '5', '1' };
+    int i, j, tmp;
+
+    bubble_md_1(cards, 5);
+
+    //bubble_md_2(cards, 5);
 
     printf("%s\n", cards);
 
